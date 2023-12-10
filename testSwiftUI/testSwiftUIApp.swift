767c86a62b93da5a33cc969702efc8e4b6a98959
DonShan <msdjshan47@gmail.com>
2023-12-10 20:13:10 +0530

    Initial Commit

---
+++ testSwiftUI/testSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSwiftUIApp.swift
+//  testSwiftUI
+//
+//  Created by Madushan Senavirathna on 2023-12-10.
+//
+
+import SwiftUI
+
+@main
+struct testSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
